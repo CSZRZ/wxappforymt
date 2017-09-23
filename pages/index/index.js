4541ab95f4b6dd1b628013e71c27a02d8c1a5c54
cszrz <cszrz@icloud.com>
2017-09-23 14:03:20 +0800

    建立首页和我的 2 个 tab，目前完成我的界面搭建

---
+++ pages/index/index.js
@@ -0,0 +1,95 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  //页面初始数据
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  //监听页面加载
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  //页面初次渲染
+  onReady: function(e) {
+
+  },
+  //页面显示
+  onShow: function(e) {
+
+  },
+  //页面隐藏
+  onHide: function(e) {
+
+  },
+  //页面卸载
+  onUnload: function(e) {
+
+  },
+  onPullDownRefresh: function () {
+    // Do something when pull down.
+  },
+  onReachBottom: function () {
+    // Do something when page reach bottom.
+  },
+  onShareAppMessage: function () {
+    // return custom share data when user share.
+  },
+  onPageScroll: function () {
+    // Do something when page scroll
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+  // wx:request({
+  //   url: '/cms/thirdPartyPortalInterfaceV2/getHomePageUIFeatureList.service',
+  //   data: {
+  //     'nsRegionId': '2'
+  //   },
+  //   method: 'POST',
+  //   success: function (res) {
+  //     console.log(res.data)
+  //   }
+
+  // })
+})
