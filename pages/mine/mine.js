89ae7516476e77cec71c0b3c8861e53cbdc98fc1
cszrz <cszrz@icloud.com>
2017-09-24 22:07:13 +0800

    我的界面加上地区选择器。

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -3,7 +3,9 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    area: ['苏州市区', '吴江', '太仓', '常熟'],
+    areaIndex: 0
   },
   onLoad: function () {
     this.setData({
@@ -11,5 +13,14 @@ Page({
         return util.formatTime(new Date(log))
       })
     })
+  },
+  //选择区域
+  onAreaChange: function (e) {
+    this.setData({
+      areaIndex: e.detail.value
+    });
+  },
+  tapName: function (event) {
+    console.log(event)
   }
 })
