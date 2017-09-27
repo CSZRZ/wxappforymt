aaade39a2c054644b88c6a852f05005952f063ac
cszrz <cszrz@icloud.com>
2017-09-27 22:43:02 +0800

    新增预约列表

---
+++ pages/mine/yuyue/yuyue.js
@@ -0,0 +1,75 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList: [{'title':'炫动卡通', 'subtitle':'魔禁猎人(1)', 'time':'2017-09-23 23:40'},
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(2)', 'time': '2017-09-23 23:40' },
+      {'title': '炫动卡通', 'subtitle': '魔禁猎人(3)', 'time': '2017-09-23 23:40' },
+      {'title': '炫动卡通', 'subtitle': '魔禁猎人(4)', 'time': '2017-09-23 23:40' },
+      {'title': '炫动卡通', 'subtitle': '魔禁猎人(5)', 'time': '2017-09-23 23:40' },
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(6)', 'time': '2017-09-23 23:40' },
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(7)', 'time': '2017-09-23 23:40' },
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(8)', 'time': '2017-09-23 23:40' },
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(9)', 'time': '2017-09-23 23:40' },
+      { 'title': '炫动卡通', 'subtitle': '魔禁猎人(10)', 'time': '2017-09-23 23:40' }]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
